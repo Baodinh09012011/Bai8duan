cbc2c7cfc01a1e9e66688dffa3e7e257d8aa780a
Baodinh09012011 <dongxuandiepxuyenmoc@gmail.com>
2024-12-01 03:25:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as s
+
+with s.sidebar:
+  s.image('https://media.yeah1.com/files/ngoctran/2022/07/01/289693821_582015943280803_2102006602626651935_n-205941.jpg')
+  s.write("Nguyen Thanh Tung")
+  s.write("nghe danh: Son Tung Mtp")
+  s.write('nghe nghiep ca si')
+  s.image('https://tse3.mm.bing.net/th?id=OIP.ButE6cItIISGTnNBKV9WYgEyDM&pid=Api&P=0&h=180')
+  s.write('Nguyen Duc Cuong')
+  s.write("nghe danh : Den Vau")
+  s.write("nghe nghiep : ca si")
+
+
+
+s.video('https://youtu.be/FN7ALfpGxiI')
+s.audio('https://zingmp3.vn/bai-hat/Lac-Troi-Son-Tung-M-TP/ZW78DIEO.html')
+s.video('https://youtu.be/KKc_RMln5UY')
+s.audio('https://soundcloud.com/den1305/loi-nho?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing')
+
